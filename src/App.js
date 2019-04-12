b176bc4ce5301b9cc09da7d8fad5784a58f707d7
Estultz731 <elizablue731@gmail.com>
2019-04-12 10:23:18 -0400

    added html

--- src/App.js
+++ src/App.js
@@ -14,6 +14,28 @@ class App extends Component {
     return (
       <>
         <Nav />
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12 col-lg-4">
+              <div className="card">
+                <div className="top"><h2>Lorem</h2></div>
+                <div className="bottom"><img className="picture" src="./images/construction.jpg" /></div>
+              </div>
+            </div>
+            <div className="col-sm-12 col-lg-4">
+              <div className="card">
+                <div className="top"><h2>Ipsum</h2></div>
+                <div className="bottom"><img className="picture" src="./images/cranes.jpg" /></div>
+              </div>
+            </div>
+            <div classNam="col-sm-12 col-lg-4">
+              <div className="card">
+                <div className="top"><h2>Nunc</h2></div>
+                <div className="bottom"><img className="picture" src="./images/sparks.jpg" /></div>
+              </div>
+            </div>
+          </div>
+        </div>
       </>
     );
   }
